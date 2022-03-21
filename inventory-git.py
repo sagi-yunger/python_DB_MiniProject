c7e71dbe69ffef6e8c0f02d46378d47098c9258c
sagi <Dxsagixd@gmail.com>
2022-03-21 18:48:14 +0200

    added the following functions : connect, curser, commit+close and read from the DB

--- inventory-git.py
+++ inventory-git.py
@@ -2,3 +2,38 @@ import sqlite3
 import datetime
 
 
+
+PATH = "C:\\sagi\\jupyter-adi\\sql\\GIT\\"
+FILENAME = "Inventory.db"
+FILE = PATH + FILENAME
+
+def set_sql_connect():
+    con = sqlite3.connect(FILE)
+    print(f"connected")
+    return con
+
+def set_sql_curser(con):
+    cur = sqlite3.Cursor(con)
+    print(f"curser up")
+    return cur
+
+def sql_close(con):
+    con.commit()
+    print(f"commited change in DB")
+    con.close()
+    print(f"closed DB")
+
+def read_from_sql(cur):
+    query = "SELECT * FROM Inventory"
+    data = cur.execute(query).fetchall()
+    print(f"{data}")
+
+
+
+
+
+
+con = set_sql_connect()
+cur = set_sql_curser(con)
+read_from_sql(cur)
+sql_close(con)
