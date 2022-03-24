c366f345bae460c5e5cbd21a7143418f3c3462d3
sagi <Dxsagixd@gmail.com>
2022-03-24 17:28:35 +0200

    added a touch of html to the flask app

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 from flask import Flask
 from inventory_git import *
 
@@ -6,15 +7,30 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello Devops Class</h1>'
+    html ="""
+    <html><body><ul>
+    <h1>Hello Devops Class</h1> 
+    <form action="/inventory" method="post" enctype="text/plain"> 
+    <button>Inventory List</button>
+    </form>
+"""
+    html+="</ul></body></html>"
+    return f'{html}'
 
 @app.route('/user/<name>')
 def user(name):
     return f'<h1>Hello, {name}!</h1>'
 
-@app.route('/inventory/')
+@app.route('/inventory')
 def inventory():
-    return f'{read_from_sql()}'
+    html="<html><body><ul>"  
+    dct = read_from_sql()
+    for line in dct:
+        html+=f"<li>{line}</li>"
+    html+="</ul></body></html>"
+    return f'{html}'
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
