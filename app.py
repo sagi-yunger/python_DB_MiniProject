17b9027a4c6bd08a27ff87fbf1323ed5d863bc34
sagi <Dxsagixd@gmail.com>
2022-03-24 20:24:29 +0200

    added add timedate module

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 
 from flask import Flask
 from inventory_git import *
+import datetime
 
 app = Flask(__name__)
+x = datetime.datetime.now() ## store todays full date and time in var x with use of datetime module 
+Today = x.strftime("%m//%d//%Y") ## make var x only date in format MM/DD/YYYY with use of datetime module and store it in Today var 
 
 
 @app.route('/')
@@ -12,6 +15,9 @@ def index():
     <h1>Hello Devops Class</h1> 
     <form action="/inventory" method="post" enctype="text/plain"> 
     <button>Inventory List</button>
+    </form>
+    <form action="/add" method="get" enctype="text/plain"> 
+    <button>add to List</button>
     </form>
 """
     html+="</ul></body></html>"
@@ -30,6 +36,10 @@ def inventory():
     html+="</ul></body></html>"
     return f'{html}'
 
+@app.route('/add')
+def add(Today):
+    result = add_item('cow','farm animal','100','1000',Today)
+    return f'{result}'
 
 
 if __name__ == '__main__':
